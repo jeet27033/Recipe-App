6b2a4e94281a73561850f5e6b119d03e6a24273b
jeet27033 <jeet.patel@capillarytech.com>
2024-12-20 17:48:54 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React,{Profiler} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const onRenderCallback = (
+  id, 
+  phase, 
+  actualDuration, 
+  baseDuration, 
+  startTime, 
+  commitTime, 
+) => {
+  console.log({ id, phase, actualDuration, baseDuration, startTime, commitTime});
+};
+
+
 root.render(
   <React.StrictMode>
+    <Profiler id="App" onRender={onRenderCallback}>
     <App />
+  </Profiler>
   </React.StrictMode>
 );
